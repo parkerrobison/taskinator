9f6392676154ba090a1f8d9d7fc4b17d181fc273
probison <parker.robison@hotmail.com>
2020-05-19 07:35:30 -0600

    changed click listener to a submit listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
+//this line is selecting the entire task form id
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+
+    // this prevents the page from refreshing every time the form is submitted.
+    event.preventDefault();
+
     //this line creates a new <li> with the variable name listItemEl
     var listItemEl = document.createElement("li");
     //this assigns that variable the CSS styling from task item
@@ -12,6 +17,6 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl);
 }
 
-// this line activates a function when the button is clicked
-buttonEl.addEventListener("click", createTaskHandler); 
+// this line was a click listener but we upadated it to be a submit listener, which activates when a button with "submit" gets clicked or by pressing enter.
+formEl.addEventListener("submit", createTaskHandler); 
 
