96a7356e873f1dd0452fbcf05e1fc82d4581a3ec
probison <parker.robison@hotmail.com>
2020-05-20 20:47:17 -0600

    added to do and in progress columns. Added delete and edit buttons to new list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //this line is selecting the entire task form id
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 var taskFormHandler = function(event) {
     // this prevents the page from refreshing every time the form is submitted.
@@ -32,18 +33,68 @@ var listItemEl = document.createElement("li");
 //this assigns that variable the CSS styling from task item
 listItemEl.className = "task-item";
 
+//add task id as a custom attribute
+listItemEl.setAttribute("data-task-id", taskIdCounter);
+
 //create div to hold task info and add to list item
 var taskInfoEl = document.createElement("div");
 // give it a class name
 taskInfoEl.className = "task-info";
 // add HTML content to div
 taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
 listItemEl.appendChild(taskInfoEl);
+// add entire list item to list
 
-    // add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);
+var taskActionsEl = createTaskActions(taskIdCounter);
+listItemEl.appendChild(taskActionsEl);
+
+tasksToDoEl.appendChild(listItemEl);
+
+// increase task counter for next uniwue id
+taskIdCounter++;
 }
+
+var createTaskActions = function(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+
+    //create edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+
+    //create delete button
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(statusSelectEl);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+
+    for (var i = 0; i < statusChoices.length; i++) {
+        //create option element
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+
+        //append to select
+        statusSelectEl.appendChild(statusOptionEl);
+    }
+
+    return actionContainerEl;
+};
 
 // this line was a click listener but we upadated it to be a submit listener, which activates when a button with "submit" gets clicked or by pressing enter.
 formEl.addEventListener("submit", taskFormHandler); 
