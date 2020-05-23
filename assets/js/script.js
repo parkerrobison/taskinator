e62e86fb635e937d677fa15de165ce492ae8c81b
probison <parker.robison@hotmail.com>
2020-05-22 21:30:22 -0600

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -288,45 +288,19 @@ var saveTasks = function() {
 }
 
 var loadTasks = function(){
-     var tasks = localStorage.getItem("tasks");
-    if (tasks === null) {
-    tasks = []; return false
+     var savedTasks = localStorage.getItem("tasks");
+    if (!savedTasks) { 
+    return false;
 }
-    tasks = JSON.parse(tasks);
-    
-    for (i = 0; i < tasks.length; i++) {
-        
-        tasks[i].id = taskIdCounter;
-        var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.setAttribute("data-task-id", tasks[i].id);
-        listItemEl.setAttribute("draggable", "true");
+    savedTasks = JSON.parse(savedTasks);
 
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-        taskInfoEl.innerHTML = "<h3 class ='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";    
-        listItemEl.appendChild(taskInfoEl);
-        
-
-        var taskActionsEl = createTaskActions(tasks[i].id);
-        listItemEl.appendChild(taskActionsEl);
-
-        if (tasks[i].status ==="to do") {
-        listItemEl.querySelector("select[name ='status-change']").selectedIndex = 0;
-        tasksToDoEl.appendChild(listItemEl);
-        }
-        else if (tasks[i].status === "in progress") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            tasksInProgressEl.appendChild(listItemEl);
-        }
-        else if (tasks[i].status === "completed") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-        taskIdCounter++;
-        console.log(listItemEl)
-
+    //loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+        //pass each task object into the 'createTaskEl()' function
+        createTaskEl(savedTasks[i]);
     }
+    
+    
 }
 // get task items for localStorage
 // convert tasks from the stringified format back into an array of objects
