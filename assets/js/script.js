0defdce1052ef185b48c42317ba9ac16a2d74df5
probison <parker.robison@hotmail.com>
2020-05-22 20:57:07 -0600

    tasks now persist after refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -286,6 +286,52 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 
 }
+
+var loadTasks = function(){
+     var tasks = localStorage.getItem("tasks");
+    if (tasks === null) {
+    tasks = []; return false
+}
+    tasks = JSON.parse(tasks);
+    
+    for (i = 0; i < tasks.length; i++) {
+        
+        tasks[i].id = taskIdCounter;
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+        listItemEl.setAttribute("data-task-id", tasks[i].id);
+        listItemEl.setAttribute("draggable", "true");
+
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class ='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";    
+        listItemEl.appendChild(taskInfoEl);
+        
+
+        var taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+
+        if (tasks[i].status ==="to do") {
+        listItemEl.querySelector("select[name ='status-change']").selectedIndex = 0;
+        tasksToDoEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "completed") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+        taskIdCounter++;
+        console.log(listItemEl)
+
+    }
+}
+// get task items for localStorage
+// convert tasks from the stringified format back into an array of objects
+// iterate through tasks array and create task elements on the page from it
+
 // this line was a click listener but we upadated it to be a submit listener, which activates when a button with "submit" gets clicked or by pressing enter.
 formEl.addEventListener("submit", taskFormHandler); 
 
@@ -300,3 +346,5 @@ pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
+
+loadTasks();
